8398f87d2d5deb098852b2a7b9951fa8a5534594
Jordi Chulia <jorchube.dev@gmail.com>
2023-02-04 23:56:42 +0100

    add /workspaces path to git safe directories

--- gnome-43-devel/on_create.sh
+++ gnome-43-devel/on_create.sh
@@ -5,3 +5,4 @@ set -e
 dnf -y install python3-gobject libadwaita-devel gtk4-devel gobject-introspection-devel
 pip install pygobject gengir
 gengir --gtk 4
+git config --global --add safe.directory /workspaces/*
