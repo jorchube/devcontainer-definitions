6559c4ec18254623c2d3990dc8ec5f9054447665
Jordi Chulia <jorchube.dev@gmail.com>
2023-02-05 00:15:36 +0100

    Fixed permissions

--- gnome-43-devel/on_create.sh
+++ gnome-43-devel/on_create.sh
@@ -5,4 +5,3 @@ set -e
 dnf -y install python3-gobject libadwaita-devel gtk4-devel gobject-introspection-devel
 pip install pygobject gengir
 gengir --gtk 4
-git config --global --add safe.directory /workspaces/*
