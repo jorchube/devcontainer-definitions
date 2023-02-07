182565b595af1c857a1bc203b7dc8586b6539cda
Jordi Chulia <jorchube.dev@gmail.com>
2023-02-07 22:57:34 +0100

    Added git config placeholders

--- gnome-43-python-devel/on_create.sh
+++ gnome-43-python-devel/on_create.sh
@@ -6,4 +6,7 @@ dnf -y install vim make python3-gobject libadwaita-devel gtk4-devel gobject-intr
 pip install pygobject gengir
 gengir --gtk 4
 
+git config --global user.name "Name"
+git config --global user.email "Email"
+
 echo "export PS1='\e[01;34m\][\w]\e[00;01m\]$\e[00m\] '" >> ~/.bashrc
