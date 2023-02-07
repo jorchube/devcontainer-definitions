f450e5a9a239a193741d6553d9abdd6af6786413
Jordi Chulia <jorchube.dev@gmail.com>
2023-02-07 22:11:58 +0100

    Tweaks

--- gnome-43-python-devel/on_create.sh
+++ gnome-43-python-devel/on_create.sh
@@ -2,6 +2,8 @@
 
 set -e
 
-dnf -y install python3-gobject libadwaita-devel gtk4-devel gobject-introspection-devel
+dnf -y install vim make python3-gobject libadwaita-devel gtk4-devel gobject-introspection-devel
 pip install pygobject gengir
 gengir --gtk 4
+
+echo "export PS1='\e[01;34m\][\w]\e[00;01m\]$\e[00m\] '" >> ~/.bashrc
