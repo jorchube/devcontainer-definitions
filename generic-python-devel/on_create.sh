663b9965ded9ba86c6a11710810814a20ff72df6
Jordi Chulia <jorchube.dev@gmail.com>
2023-03-19 22:40:46 +0100

    Simplify prompt

--- generic-python-devel/on_create.sh
+++ generic-python-devel/on_create.sh
@@ -6,4 +6,4 @@ dnf -y install git vim make pip
 pip install pipenv
 
 echo ". /usr/share/git-core/contrib/completion/git-prompt.sh" >> ~/.bashrc
-echo "export PS1='[\[\e[01;34m\]\W\[\e[01;35m\]\$(__git_ps1 \" %s\")\[\e[00;00m\]]$\[\e[00m\] '" >> ~/.bashrc
+echo "export PS1='[\[\e[34m\]\W\[\e[35m\]\$(__git_ps1 \" %s\")\[\e[00m\]]$\[\e[00m\] '" >> ~/.bashrc
