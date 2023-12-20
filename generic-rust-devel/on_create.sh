e3f1b285d43c03e6c98864bbb4f464c9585eaa5a
Jordi Chulia <jorchube.dev@gmail.com>
2023-12-20 18:37:23 +0100

    Define env variable to install cargo packages in /usr/local by default

--- generic-rust-devel/on_create.sh
+++ generic-rust-devel/on_create.sh
@@ -3,7 +3,7 @@
 set -e
 
 dnf -y install git vim rust rust-src cargo
-cargo install --root /usr/local rustfmt
+cargo install rustfmt
 
 echo ". /usr/share/git-core/contrib/completion/git-prompt.sh" >> ~/.bashrc
 echo "export PS1='[\[\e[34m\]\W\[\e[35m\]\$(__git_ps1 \" %s\")\[\e[00m\]]$\[\e[00m\] '" >> ~/.bashrc
