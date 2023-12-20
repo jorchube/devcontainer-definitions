b9047214aa20e3b8881f28f78cc46f03c98fa9c1
Jordi Chulia <jorchube.dev@gmail.com>
2023-12-20 17:52:22 +0100

    Added a generic rust setup

---
+++ generic-rust-devel/on_create.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+set -e
+
+dnf -y install git vim rust rust-src cargo
+
+echo ". /usr/share/git-core/contrib/completion/git-prompt.sh" >> ~/.bashrc
+echo "export PS1='[\[\e[34m\]\W\[\e[35m\]\$(__git_ps1 \" %s\")\[\e[00m\]]$\[\e[00m\] '" >> ~/.bashrc
