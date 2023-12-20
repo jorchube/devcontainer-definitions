8ea31b92dde15d3c7ad4ba82d4d00520664d1fcf
Jordi Chulia <jorchube.dev@gmail.com>
2023-12-20 18:29:35 +0100

    Install rustfmt on creation

--- generic-rust-devel/on_create.sh
+++ generic-rust-devel/on_create.sh
@@ -3,6 +3,7 @@
 set -e
 
 dnf -y install git vim rust rust-src cargo
+cargo install --root /usr/local rustfmt
 
 echo ". /usr/share/git-core/contrib/completion/git-prompt.sh" >> ~/.bashrc
 echo "export PS1='[\[\e[34m\]\W\[\e[35m\]\$(__git_ps1 \" %s\")\[\e[00m\]]$\[\e[00m\] '" >> ~/.bashrc
